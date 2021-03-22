c36275cefbf8366e67766cc9ca1794b9a600ab61
Gillian Louise <gillianhughes2017@gmail.com>
2021-03-21 20:14:24 -0500

    Add menu animation

--- assets/js/ui.js
+++ assets/js/ui.js
@@ -1,11 +1,11 @@
 /* Set the width of the side navigation to 250px and the left margin of the page content to 250px */
 function openNav() {
-    document.getElementById("mySidenav").style.width = "250px";
-    document.getElementById("main").style.marginRight = "250px";
+    document.getElementById("menu").classList.add("menu-is-open");
+    document.getElementById("content").classList.add("menu-is-open");
 }
 
 /* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
 function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
-    document.getElementById("main").style.marginRight = "0";
+    document.getElementById("menu").classList.remove("menu-is-open");
+    document.getElementById("content").classList.remove("menu-is-open");
 }
