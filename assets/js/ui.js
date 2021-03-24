d4c04a8d04ef4259e1f520aab789d3da5399c7de
lisandroct <lisandroct@gmail.com>
2021-03-23 23:05:03 -0500

    Style footer

--- assets/js/ui.js
+++ assets/js/ui.js
@@ -1,7 +1,7 @@
 /* Set the width of the side navigation to 250px and the left margin of the page content to 250px */
 function openNav() {
     document.getElementById("menu").classList.add("menu-is-open");
-    var contents = document.getElementsByClassName("content");
+    var contents = document.getElementsByClassName("content-container");
     for (var i = 0; i < contents.length; i++) {
         contents[i].classList.add("menu-is-open");
     }
@@ -10,7 +10,7 @@ function openNav() {
 /* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
 function closeNav() {
     document.getElementById("menu").classList.remove("menu-is-open");
-    var contents = document.getElementsByClassName("content");
+    var contents = document.getElementsByClassName("content-container");
     for (var i = 0; i < contents.length; i++) {
         contents[i].classList.remove("menu-is-open");
     }
